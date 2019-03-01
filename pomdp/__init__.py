548a150e88c79afe23d1001a7ce56a1ec2f1dc6a
angel-manuel <angel_manuel_martin@yahoo.es>
2019-03-01 19:43:57 +0100

    Initial commit

---
+++ pomdp/__init__.py
@@ -0,0 +1,3 @@
+
+__author__ = "Ángel Manuel Martín Canto"
+__version__ = "0.1.0"
---
+++ pomdp/__main__.py
@@ -0,0 +1,6 @@
+
+from pomdp import __version__, __author__
+
+if __name__ == "__main__":
+    print("pomdp: A POMDP solver {} by {}".format(__version__, __author__))
+    pass
